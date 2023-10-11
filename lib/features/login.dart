56e94430a00d413ec94958bb2982507513ea4c4f
Heaveee <ariezsintos06@gmail.com>
2023-10-11 10:14:37 +0800

    init

---
+++ lib/features/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+@override
+Widget build(BuildContext context){
+  return Scaffold(
+    body: AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light,
+      child: GestureDetector(
+        child:  Stack(
+          children: <Widget>[],
+        ),
+      ),
+    ),
+  )
+}
+
+}
