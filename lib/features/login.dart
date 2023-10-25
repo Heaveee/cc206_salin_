a4f767a9c26ef914177adc31afc82e13cc237f61
Heaveee <ariezsintos06@gmail.com>
2023-10-25 11:43:59 +0800

    features updated

--- lib/features/login.dart
+++ lib/features/login.dart
@@ -1,19 +1,69 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(SalinApp());
+}
 
-class Signup extends StatelessWidget{
+class SalinApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: Column(
-        children: [
-          //TextField(),
-          //Icon(),
-          //Container(),
-          //Image()
-          //StatelessWidget
-        ],
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.yellow,
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.red),
       ),
-    )
+      home: SalinLoginForm(),
+    );
+  }
+}
+
+class SalinLoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Salin Login"),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/salin_logo.png', // Replace with the actual path to your logo
+                width: 100,
+                height: 100,
+              ),
+              Text(
+                "Log In to Salin",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                decoration: InputDecoration(labelText: "Username"),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(labelText: "Password"),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // din makadto button if ma pindot
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                ),
+                child: Text("Log In",
+                    style:
+                        TextStyle(color: Color.fromARGB(255, 255, 255, 255))),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/features/signup.dart
+++ lib/features/signup.dart
@@ -1,19 +1,76 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(SignUpApp());
+}
 
-class Signup extends StatelessWidget{
+class SignUpApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: Column(
-        children: [
-          //TextField(),
-          //Icon(),
-          //Container(),
-          //Image()
-          //StatelessWidget
-        ],
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.yellow,
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: Colors.red), // Set accent color to red
       ),
-    )
+      home: SignUpForm(),
+    );
+  }
+}
+
+class SignUpForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Salin"),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/salin_logo.png', // Replace with the actual path to your logo
+                width: 100,
+                height: 100,
+              ),
+              Text(
+                "Create a Salin Account",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                decoration: InputDecoration(labelText: "Username"),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                decoration: InputDecoration(labelText: "Email"),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(labelText: "Password"),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // if din makadto ang button
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Colors.red), // Set the button color to red
+                ),
+                child: Text("Sign Up",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 252, 252,
+                            252))), // Set the button text color to yellow
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
